c5c2bace3b9b3287898ec1f6d5dc92e9f0d39b11
jbrentfostger <jbrentfoster@yahoo.com>
2021-06-23 15:24:29 -0400

    Added code for bearer token authentication

--- utils.py
+++ utils.py
@@ -14,7 +14,17 @@ or implied.
 import requests
 import json
 import logging
+import urllib
 from datetime import datetime
+
+
+class BearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
 
 
 def rest_get_json(baseURL, uri, user, password):
@@ -24,9 +34,14 @@ def rest_get_json(baseURL, uri, user, password):
     }
     appformat = 'application/yang-data+json'
     headers = {'content-type': appformat, 'accept': appformat}
+    if user == 'BEARER_TOKEN':
+        auth = BearerAuth(password)
+    else:
+        auth = (user, password)
+    # headers = {"Authorization": "Bearer MYREALLYLONGTOKENIGOT"}
     restURI = baseURL + uri
     try:
-        r = requests.get(restURI, headers=headers, proxies=proxies, auth=(user, password), verify=False)
+        r = requests.get(restURI, headers=headers, proxies=proxies, auth=auth, verify=False)
         # logging.info('The API response for URL {} is:\n{}'.format(restURI, json.dumps(r.json(), separators=(",",":"), indent=4)))
         if r.status_code == 200:
             return json.dumps(r.json(), indent=2)
@@ -34,10 +49,12 @@ def rest_get_json(baseURL, uri, user, password):
             thejson = json.loads(json.dumps(r.json(), indent=2))
             errormessage = thejson.get('rc.errors').get('error').get('error-message')
             logging.info('error message is: ' + errormessage)
-            raise errors.InputError(restURI, "HTTP status code: " + str(r.status_code), "Error message returned: " + errormessage)
+            raise errors.InputError(restURI, "HTTP status code: " + str(r.status_code),
+                                    "Error message returned: " + errormessage)
     except Exception as err:
         # logging.error('Exception raised: ' + str(type(err)) + '\nURL: {}\n{}\n{}'.format(err.expression, err.statuscode,err.message))
         return
+
 
 def rest_get_schema(baseURL, uri, user, password):
     proxies = {
@@ -47,8 +64,12 @@ def rest_get_schema(baseURL, uri, user, password):
     # appformat = 'application/yang-data+json'
     # headers = {'content-type': appformat, 'accept': appformat}
     restURI = baseURL + uri
+    if user == 'BEARER_TOKEN':
+        auth = BearerAuth(password)
+    else:
+        auth = (user, password)
     try:
-        r = requests.get(restURI, proxies=proxies, auth=(user, password), verify=False)
+        r = requests.get(restURI, proxies=proxies, auth=auth, verify=False)
         # logging.info('The API response for URL {} is:\n{}'.format(restURI, json.dumps(r.json(), separators=(",",":"), indent=4)))
         if r.status_code == 200:
             return r.text
@@ -59,27 +80,30 @@ def rest_get_schema(baseURL, uri, user, password):
         #     logging.info('error message is: ' + errormessage)
         #     raise errors.InputError(restURI, "HTTP status code: " + str(r.status_code), "Error message returned: " + errormessage)
     except Exception as err:
-        # logging.error('Exception raised: ' + str(type(err)) + '\nURL: {}\n{}\n{}'.format(err.expression, err.statuscode,err.message))
+        logging.error('Exception raised: ' + str(type(err)) + '\nURL: {}\n{}\n{}'.format(err.expression, err.statuscode,err.message))
         return
 
+
 def rest_post_json(baseURL, uri, thejson, user, password):
-        proxies = {
-            "http": None,
-            "https": None,
-        }
-        appformat = 'application/yang-data+json'
-        headers = {'content-type': appformat, 'accept': appformat}
-        restURI = baseURL + uri
-        try:
-            r = requests.post(restURI, data=thejson, headers=headers, proxies=proxies, auth=(user, password),verify=False)
-            # logging.info('The API response for URL {} is:\n{}'.format(restURI, json.dumps(r.json(), separators=(",",":"), indent=4)))
-            if r.status_code == 200:
-                return json.dumps(r.json(), indent=2)
-            else:
-                thejson = json.loads(json.dumps(r.json(), indent=2))
-                errormessage = thejson.get('rc.errors').get('error').get('error-message')
-                logging.info('error message is: ' + errormessage)
-                raise errors.InputError(restURI, "HTTP status code: " + str(r.status_code), "Error message returned: " + errormessage)
-        except Exception as err:
-            logging.error('Exception raised: ' + str(type(err)) + '\nURL: {}\n{}\n{}'.format(err.expression, err.statuscode,err.message))
-            return
+    proxies = {
+        "http": None,
+        "https": None,
+    }
+    appformat = 'application/yang-data+json'
+    headers = {'content-type': appformat, 'accept': appformat}
+    restURI = baseURL + uri
+    try:
+        r = requests.post(restURI, data=thejson, headers=headers, proxies=proxies, auth=(user, password), verify=False)
+        # logging.info('The API response for URL {} is:\n{}'.format(restURI, json.dumps(r.json(), separators=(",",":"), indent=4)))
+        if r.status_code == 200:
+            return json.dumps(r.json(), indent=2)
+        else:
+            thejson = json.loads(json.dumps(r.json(), indent=2))
+            errormessage = thejson.get('rc.errors').get('error').get('error-message')
+            logging.info('error message is: ' + errormessage)
+            raise errors.InputError(restURI, "HTTP status code: " + str(r.status_code),
+                                    "Error message returned: " + errormessage)
+    except Exception as err:
+        logging.error('Exception raised: ' + str(type(err)) + '\nURL: {}\n{}\n{}'.format(err.expression, err.statuscode,
+                                                                                         err.message))
+        return
